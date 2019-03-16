e9ae87e7335efe9d8d2961b20365100eac078200
Sai Ganesh <44020019+luckyganesh@users.noreply.github.com>
2019-03-16 18:44:54 +0530

    [Durga,Ganesh] Created PrintBoard function in main.
    It will create the board in command line.

--- src/com/games/_2048/Main.java
+++ src/com/games/_2048/Main.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        Game game = new Game("lela",4);
+        Game game = new Game("leela",4);
         System.out.println(game.moveLeft(new ArrayList<>(Arrays.asList(2, 2, 4, 2))));
         System.out.println(game.moveLeft(new ArrayList<>(Arrays.asList(2, 4, 4, 2))));
         System.out.println(game.moveLeft(new ArrayList<>(Arrays.asList(4, 2, 4, 2))));
@@ -15,7 +15,23 @@ public class Main {
         System.out.println(game.moveLeft(new ArrayList<>(Arrays.asList(2, 2, 0, 0))));
 
         System.out.println(game.moveLeft(new ArrayList<>(Arrays.asList(0, 0, 0, 0))));
+        ArrayList<ArrayList<Integer>> board = new ArrayList<>();
+        board.add(new ArrayList<>(Arrays.asList(2, 2, 0, 0)));
+        board.add(new ArrayList<>(Arrays.asList(2, 2, 0, 0)));
+        board.add(new ArrayList<>(Arrays.asList(2, 2, 0, 0)));
+        board.add(new ArrayList<>(Arrays.asList(2, 2, 0, 0)));
+        printBoard(board);
     }
 
 
+    public static void printBoard(ArrayList<ArrayList<Integer>> board) {
+        System.out.println("+---+---+---+---+");
+        for (ArrayList<Integer> row : board) {
+            System.out.print("|");
+            for (Integer integer : row) {
+                System.out.print(" " + ((integer != 0) ? integer : " ") + " |");
+            }
+            System.out.println("\n+---+---+---+---+");
+        }
+    }
 }
